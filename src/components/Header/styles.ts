31cc067dba5677094bf35b5ccbb6a354a1dde003
MyTruQs <raulfernandes.rm@gmail.com>
2024-11-23 19:24:04 -0300

    Estrutura visual - Componente: Summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,7 @@ export const HeaderContainer = styled.header`
 
 export const HeaderContent = styled.div`
     width: 100%;
-    max-width: 1220px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: 0 1.5rem;
 
